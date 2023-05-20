f35b7c1df6928a0b148eb852bd8d9ad0dde4f9c7
GeraldTgit <117510768+GeraldTgit@users.noreply.github.com>
2023-05-20 18:36:10 +0800

    Update aharts_cust_form.py
    Added function to be able to search for customer information by entering customer id.
    Added function to save data to csv file.
    It now also able to edit existing customer information base on customer id.

--- aharts_cust_form.py
+++ aharts_cust_form.py
@@ -1,11 +1,12 @@
 # Anthony's Home Appliance Repair Ticketing System
 import PyQt5.QtWidgets as qtw
 import PyQt5.QtGui as qtg
+import subprocess
+import shutil
 import os
 import sys
-import shutil
 import csv
-import my_design
+import tempfile
 
 class CustWindow(qtw.QWidget):
     def __init__(self):
@@ -22,6 +23,36 @@ class CustWindow(qtw.QWidget):
 
         # last/hisghest customer id
         customer_db = temp_db+"customer.csv"
+
+        # Getting things ready
+        # Check if the customer.csv file exists in the customer_db directory
+        if not os.path.isfile(customer_db):
+            # Create a new customer.csv file with headers
+            headers = ['Customer ID', 'First Name', 'Last Name', 'Contact Number', 'Email', 'Home Address', 'ID Type', 'ID Path']
+            with open(customer_db, 'w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(headers)
+
+        # Look for customer information based on tracking number
+        def look_up_cust():
+            #uf_cust_fname_entry.setFont(qtg.QFont('Arial', 9, italic=False))
+            #uf_cust_fname_entry.setStyleSheet("color: black")
+            # Get the search query from the entry box
+            search_query = uf_cust_tnum_entry.text()
+            # Open the CSV file and search for the customer information
+            with open(customer_db, 'r') as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    if search_query in row:
+                        # Customer information found, do something with it
+                        uf_cust_fname_entry.setText(row[1])
+                        uf_cust_lname_entry.setText(row[2])
+                        uf_cust_cnum_entry.setText(row[3])
+                        uf_cust_Email_entry.setText(row[4])
+                        uf_cust_Hadd_entry.setText(row[5])
+                        uf_cust_pid_entry.setCurrentText(row[6])
+                        uf_cust_aid.setText(row[7])
+                        break
 
         # Reset input boxes design
         def reset_design_fname():
@@ -57,6 +88,17 @@ class CustWindow(qtw.QWidget):
         uf_cust_info = qtw.QLabel("Customer Information: ")
         uf_cust_info.setFont(qtg.QFont('Arial', 15))
         self.layout().addWidget(uf_cust_info)
+
+        # Userform header Customer ID number
+        uf_cust_tnum = qtw.QLabel("Tracking Number: ")
+        uf_cust_tnum.setFont(qtg.QFont('Arial', 9))
+        self.layout().addWidget(uf_cust_tnum)
+
+        # Userform Entry box for customer First Name
+        uf_cust_tnum_entry = qtw.QLineEdit()
+        uf_cust_tnum_entry.textChanged.connect(look_up_cust)
+        uf_cust_tnum_entry.setObjectName("fname_field")
+        self.layout().addWidget(uf_cust_tnum_entry)
 
         # Userform header Customer First Name
         uf_cust_fname = qtw.QLabel("First Name: ")
@@ -163,6 +205,10 @@ class CustWindow(qtw.QWidget):
         uf_cust_clear_button = qtw.QPushButton("Clear All", clicked = lambda: clear_all())
         self.layout().addWidget(uf_cust_clear_button)
 
+        # refresh page
+        reload_button = qtw.QPushButton("Back to Main", clicked = lambda: reload())
+        self.layout().addWidget(reload_button)
+
         # Upload ID
         def open_file():
             #app = qtw.QApplication(sys.argv)
@@ -175,7 +221,6 @@ class CustWindow(qtw.QWidget):
             if file_name:
                 uf_cust_aid.setText(f"ID path: {file_name[0]}")
                 uf_cust_aid.setStyleSheet("color: blue")
-                
 
         # This part is very IMPORTANT!!
         self.show()
@@ -194,26 +239,74 @@ class CustWindow(qtw.QWidget):
                 if uf_cust_aid.text() != "Identification":
                     shutil.copy(uf_cust_aid.text()[9:], temp_db+"id/")
 
-                saving_it()
-                print('OK clicked')
+                saving_it()    
 
         def saving_it():
-            print(customer_db)
+            # Get the input values from the user form
+            first_name = uf_cust_fname_entry.text()
+            last_name = uf_cust_lname_entry.text()
+            contact_number = uf_cust_cnum_entry.text()
+            email = uf_cust_Email_entry.text()
+            home_address = uf_cust_Hadd_entry.text()
+            id_type = uf_cust_pid_entry.currentText()
+            id_path = uf_cust_aid.text()
+
             with open(customer_db, 'r') as file:
                 reader = csv.reader(file)
                 # skip header row if it exists
                 if csv.Sniffer().has_header(file.read(1024)):
                     file.seek(0)
                     next(reader)
-                # get max value from firs\CustWindowt column
-                highest_cust_id = max(int(row[0]) for row in reader)
 
+                try:
+                    # Get the max value from the first column or assign 0 if the reader is empty
+                    highest_cust_id = max(int(row[0]) for row in reader)
+                except ValueError:
+                    highest_cust_id = 0
+
+            # Assign a value of 1 if no existing entries
             new_entry_cust_id = highest_cust_id + 1
-            print(f"New customer recorded; CustomerID: {new_entry_cust_id}")
+            
+            # Rest of the code to update or append rows
+            with open(customer_db, 'r') as file:
+                reader = csv.reader(file)
+                rows = list(reader)
+
+                if len(rows) > 0:
+                    for row in rows:
+                        if row[0] == uf_cust_tnum_entry.text():
+                            # Customer ID already exists, update the row
+                            row[1] = first_name
+                            row[2] = last_name
+                            row[3] = contact_number
+                            row[4] = email
+                            row[5] = home_address
+                            row[6] = id_type
+                            row[7] = id_path
+                            break
+                    else:
+                        # Customer ID doesn't exist, append a new row
+                        rows.append([new_entry_cust_id, first_name, last_name, contact_number, email, home_address, id_type, id_path])
+                else:
+                    # Customer.csv is empty, add the headers and append a new row
+                    headers = ['Customer ID', 'First Name', 'Last Name', 'Contact Number', 'Email', 'Home Address', 'ID Type', 'ID Path']
+                    rows.append(headers)
+                    rows.append([new_entry_cust_id, first_name, last_name, contact_number, email, home_address, id_type, id_path])
+
+            # Write the modified data back to the file
+            with tempfile.NamedTemporaryFile(mode='w', delete=False) as temp_file:
+                with open(temp_file.name, 'w', newline='') as file:
+                    writer = csv.writer(file)
+                    writer.writerows(rows)  # Write the rows
+
+            # Replace the original file with the updated file
+            shutil.move(temp_file.name, customer_db) 
+
             clear_all()
 
         # Clear all fields button
         def clear_all():
+            uf_cust_tnum_entry.setText("")
             uf_cust_fname_entry.setText("")
             uf_cust_lname_entry.setText("")
             uf_cust_cnum_entry.setText("")
@@ -228,6 +321,20 @@ class CustWindow(qtw.QWidget):
             reset_design_pid()
             uf_cust_aid.setText("Identification")
             uf_cust_aid.setStyleSheet("color: black")
+
+        # To reload the page
+        def reload():
+            # Path to the Python script you want to rerun
+            script_path = pwd_+"/reload.py"
+
+            # Define the command to run the new script
+            new_script_command = ["python", script_path]
+
+            # Start the new script
+            subprocess.Popen(new_script_command)
+
+            # Exit the current script
+            sys.exit()
 
 if __name__ == '__main__':
     app = qtw.QApplication(sys.argv)
