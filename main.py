ec0fee19fc1bc639a2c391ba9b43def1b2182d79
GeraldTgit <117510768+GeraldTgit@users.noreply.github.com>
2023-06-12 15:03:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 # Anthony's Home Appliance Repair Ticketing System
+# Common packages
 import PyQt5.QtWidgets as qtw
 import PyQt5.QtGui as qtg
+from PyQt5.QtCore import Qt
 from PyQt5.QtCore import QTimer
-import subprocess
 import datetime
-import sys
 import os
+
+# Customize scripts
+from logs_generator import *
+from add_new_customer_backend import *
+from goto_page import *
+
+# Param checker
+setup_log_file()
+check_customer_db()
 
 current_time = datetime.datetime.now()
 current_hour = current_time.hour
@@ -26,24 +35,29 @@ class MainWindow(qtw.QWidget):
     def __init__(self):
         super().__init__()
         # System userform title
-        self.setWindowTitle("AHARTS - Menu Page")
+        self.setWindowTitle("AHARTS - Main Manu")
         # Userform layout
         self.setLayout(qtw.QVBoxLayout())
 
         # Userform header
-        uf_header = qtw.QLabel("Anthony's Home Appliance Repair Ticketing System")
+        uf_header = qtw.QLabel("Anthony's Home Appliance-Repair Ticketing System")
         uf_header.setFont(qtg.QFont('Arial', 25))
         self.layout().addWidget(uf_header)
 
-        # Userform greeting with date
-        uf_greet_header = qtw.QLabel(greetings + "                                                     Today is " + current_date)
+        # Userform greeting
+        uf_greet_header = qtw.QLabel(greetings)
         uf_greet_header.setFont(qtg.QFont('Arial', 15))
         self.layout().addWidget(uf_greet_header)
+
+        # Show date today
+        label_today = qtw.QLabel(f"Today is {current_date}")
+        label_today.setFont(qtg.QFont('Arial', 9))
+        self.layout().addWidget(label_today, alignment=Qt.AlignTop | Qt.AlignRight)
 
         # create a QLabel object to display the time
         self.uf_current_time = qtw.QLabel(self)
         self.uf_current_time.setFont(qtg.QFont('Arial', 9))
-        self.layout().addWidget(self.uf_current_time)
+        self.layout().addWidget(self.uf_current_time, alignment=Qt.AlignTop | Qt.AlignRight)
 
         # start the timer to update the label
         self.timer = QTimer()
@@ -56,11 +70,11 @@ class MainWindow(qtw.QWidget):
         self.layout().addWidget(cust_header)
 
         # call aharts_cust_form.py
-        cust_entry_button = qtw.QPushButton("Add New Customer", clicked = lambda: cust_entry_form())
+        cust_entry_button = qtw.QPushButton("Add New Customer", clicked = lambda: goto_page("aharts_cust_form.py"))
         self.layout().addWidget(cust_entry_button)
 
         # call aharts_edit_customer.py
-        cust_edit_button = qtw.QPushButton("Edit Customer Information", clicked = lambda: aharts_edit_customer())
+        cust_edit_button = qtw.QPushButton("Edit Customer Information", clicked = lambda: goto_page("aharts_edit_customer.py"))
         self.layout().addWidget(cust_edit_button)
 
         # Ticket form header
@@ -78,56 +92,20 @@ class MainWindow(qtw.QWidget):
         self.layout().addWidget(cust_header_db)
 
         # Open database button
-        cust_db_button = qtw.QPushButton("View Customer Database", clicked = lambda: open_database_viewer())
+        cust_db_button = qtw.QPushButton("View Customer Database", clicked = lambda: goto_page("database_viewer.py"))
         self.layout().addWidget(cust_db_button)
 
         # refresh page
-        reload_button = qtw.QPushButton("RELOAD PAGE", clicked = lambda: reload())
+        reload_button = qtw.QPushButton("RELOAD PAGE", clicked = lambda: goto_page("main.py"))
         reload_button.setFont(qtg.QFont('Arial', 15))
         self.layout().addWidget(reload_button)
 
         # This part is very IMPORTANT!!
         self.show()
 
-        # To call customer form
-        def cust_entry_form():
-            CloseOpen("aharts_cust_form.py")
-
-        # To call edit customer information form
-        def aharts_edit_customer():
-            CloseOpen("aharts_edit_customer.py")
-
         # To call ticket form
         def ticket_entry_form():
             print("Wala pa. Wag kang excited")
-
-
-        # To open the customer database view form
-        def open_database_viewer():
-            # Path to the Python script you want to rerun
-            script_path = pwd_+"/database_viewer.py"
-
-            # Define the command to run the new script
-            new_script_command = ["python", script_path]
-
-            # Start the new script
-            subprocess.Popen(new_script_command)
-
-        # To reload the page
-        def reload():
-            CloseOpen("reload.py")
-
-        # To close existing script and then open main.py
-        def CloseOpen(page):
-            # Path to the Python script you want to rerun
-            script_path = pwd_+"/"+page
-            # Define the command to run the new script
-            new_script_command = ["python", script_path]
-            # Start the new script
-            subprocess.Popen(new_script_command)
-            # Exit the current script
-            sys.exit()
-            
 
     # This code updates uf_current_time every seconds
     def update_time(self):
