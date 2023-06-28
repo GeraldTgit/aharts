8afb98eaeb824a189de91bdbf6b8d1c91c9b51a6
GeraldTgit <117510768+GeraldTgit@users.noreply.github.com>
2023-06-28 17:41:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 # Anthony's Home Appliance Repair Ticketing System
 # Common packages
-import PyQt5.QtWidgets as qtw
-import PyQt5.QtGui as qtg
-from PyQt5.QtCore import Qt
-from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 import datetime
+import ctypes
+import sys
 
 # Customize scripts
+# Sub-pages
+from aharts_cust_form import AddNew_CustWindow
+from aharts_edit_customer import Edit_CustWindow
+
+# Backend scripts
 from logs_generator import *
 from add_new_customer_backend import *
 from goto_page import *
 from public_backend import *
+
 
 # Param checker
 setup_log_file()
@@ -18,6 +25,9 @@ check_customer_db()
 
 current_time = datetime.datetime.now()
 current_hour = current_time.hour
+
+# Task bar icon
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid())
 
 if current_hour < 12:
     greetings = "Good morning!"
@@ -28,33 +38,31 @@ else:
 
 current_date = current_time.strftime("%A, %B %d, %Y")
 
-class MainWindow(qtw.QWidget):
+class MainForm(QWidget):
     def __init__(self):
         super().__init__()
-        # System userform title
-        self.setWindowTitle("AHARTS - Main Manu")
-        # Userform layout
-        self.setLayout(qtw.QVBoxLayout())
-        self.setWindowIcon(qtg.QIcon(tsys_icon()))
+        self.setWindowTitle("AHARTS - Main Menu")
+        self.setLayout(QVBoxLayout())
+        self.setWindowIcon(QIcon(tsys_icon()))
 
         # Userform header
-        uf_header = qtw.QLabel("Anthony's Home Appliance-Repair Ticketing System")
-        uf_header.setFont(qtg.QFont('Arial', 25))
+        uf_header = QLabel("Anthony's Home Appliance-Repair Ticketing System")
+        uf_header.setFont(QFont('Arial', 25))
         self.layout().addWidget(uf_header)
 
         # Userform greeting
-        uf_greet_header = qtw.QLabel(greetings)
-        uf_greet_header.setFont(qtg.QFont('Arial', 15))
+        uf_greet_header = QLabel(greetings)
+        uf_greet_header.setFont(QFont('Arial', 15))
         self.layout().addWidget(uf_greet_header)
 
         # Show date today
-        label_today = qtw.QLabel(f"Today is {current_date}")
-        label_today.setFont(qtg.QFont('Arial', 9))
+        label_today = QLabel(f"Today is {current_date}")
+        label_today.setFont(QFont('Arial', 9))
         self.layout().addWidget(label_today, alignment=Qt.AlignTop | Qt.AlignRight)
 
         # create a QLabel object to display the time
-        self.uf_current_time = qtw.QLabel(self)
-        self.uf_current_time.setFont(qtg.QFont('Arial', 9))
+        self.uf_current_time = QLabel(self)
+        self.uf_current_time.setFont(QFont('Arial', 9))
         self.layout().addWidget(self.uf_current_time, alignment=Qt.AlignTop | Qt.AlignRight)
 
         # start the timer to update the label
@@ -63,48 +71,72 @@ class MainWindow(qtw.QWidget):
         self.timer.start(1000) # update every 1 second
 
         # Customer form header
-        cust_header = qtw.QLabel("Customer Form")
-        cust_header.setFont(qtg.QFont('Arial', 15))
+        cust_header = QLabel("Customer Form")
+        cust_header.setFont(QFont('Arial', 15))
         self.layout().addWidget(cust_header)
 
-        # call aharts_cust_form.py
-        cust_entry_button = qtw.QPushButton("Add New Customer", clicked = lambda: goto_page("aharts_cust_form.py"))
+
+        #########################################################################
+        # BUTTONS
+        #########################################################################
+
+        # Create a button to open the other user form
+        cust_entry_button = QPushButton("Add New Customer", clicked = lambda: self.open_addnew_custwindow())
         self.layout().addWidget(cust_entry_button)
 
         # call aharts_edit_customer.py
-        cust_edit_button = qtw.QPushButton("Edit Customer Information", clicked = lambda: goto_page("aharts_edit_customer.py"))
+        cust_edit_button = QPushButton("Edit Customer Information", clicked = lambda: self.open_edit_custwindow())
         self.layout().addWidget(cust_edit_button)
 
         # Ticket form header
-        cust_header = qtw.QLabel("Ticket Form")
-        cust_header.setFont(qtg.QFont('Arial', 15))
+        cust_header = QLabel("Ticket Form")
+        cust_header.setFont(QFont('Arial', 15))
         self.layout().addWidget(cust_header)
 
         # call aharts_ticket_entry.py
-        ticket_entry_button = qtw.QPushButton("Service Ticket", clicked = lambda: ticket_entry_form())
+        ticket_entry_button = QPushButton("Service Ticket", clicked = lambda: goto_page("create_service_ticket.py"))
         self.layout().addWidget(ticket_entry_button)
 
+        # Appliance troubleshooting log
+        appliance_ts_log = QPushButton("Appliance Troubleshooting Log", clicked = lambda: goto_page("troubleshooting.py"))
+        self.layout().addWidget(appliance_ts_log)
+
         # Databases
-        cust_header_db = qtw.QLabel("Database")
-        cust_header_db.setFont(qtg.QFont('Arial', 15))
+        cust_header_db = QLabel("Database")
+        cust_header_db.setFont(QFont('Arial', 15))
         self.layout().addWidget(cust_header_db)
 
         # Open database button
-        cust_db_button = qtw.QPushButton("View Customer Database", clicked = lambda: goto_page("database_viewer.py"))
+        cust_db_button = QPushButton("View Customer Database", clicked = lambda: goto_page("database_viewer.py"))
         self.layout().addWidget(cust_db_button)
 
         # refresh page
-        reload_button = qtw.QPushButton("RELOAD PAGE", clicked = lambda: goto_page("main.py"))
-        reload_button.setFont(qtg.QFont('Arial', 15))
+        reload_button = QPushButton("RELOAD PAGE", clicked = lambda: goto_page("main.py"))
+        reload_button.setFont(QFont('Arial', 15))
         self.layout().addWidget(reload_button)
 
-        # This part is very IMPORTANT!!
-        self.show()
+        
+    ########################################################
+    # FUNCTIONS
+    ########################################################
 
-        # To call ticket form
-        def ticket_entry_form():
-            print("Wala pa. Wag kang excited")
+    def open_addnew_custwindow(self):
+        # Show the other user form and hide the main form
+        # Create an instance of the other user form
+        self.addnew_custwindow = AddNew_CustWindow()
+        # Add the other user form to the layout of the main form
+        self.layout().addLayout(self.addnew_custwindow.layout())
+        self.addnew_custwindow.show()
+        self.hide()
 
+    def open_edit_custwindow(self):
+        self.edit_custwindow = Edit_CustWindow()
+        # Add the other user form to the layout of the main form
+        self.layout().addLayout(self.edit_custwindow.layout())
+        self.edit_custwindow.show()
+        self.hide()
+
+        
     # This code updates uf_current_time every seconds
     def update_time(self):
         # get the current time as a string
@@ -112,7 +144,9 @@ class MainWindow(qtw.QWidget):
         # update the text property of the existing QLabel object
         self.uf_current_time.setText(current_time)
 
-app = qtw.QApplication([])
-mw = MainWindow()
-
-app.exec_()
+# Main application
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_form = MainForm()
+    main_form.show()
+    sys.exit(app.exec_())
