01006a52b05d28812115feb6fe71e0e27f2804df
GeraldTgit <117510768+GeraldTgit@users.noreply.github.com>
2023-07-02 15:59:29 +0800

    Update troubleshooting.py

--- troubleshooting.py
+++ troubleshooting.py
@@ -1,37 +1,40 @@
-import PyQt5.QtWidgets as qtw
-import PyQt5.QtGui as qtg
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 import pyperclip
-import sys
 
 # Customize scripts
 from database_viewer import *
-from logs_generator import *
-from public_backend import *
-from goto_page import *
-from troubleshooting_backend import *
+from backend.logs_generator import *
+from backend.public_backend import *
+from backend.goto_page import return_to_previous_page
+from backend.troubleshooting_backend import *
 
-class TroubleshootingWindow(qtw.QWidget):
-    def __init__(self):
+class TroubleshootingWindow(QWidget):
+    def __init__(self, main_window):
         super().__init__()
         self.setWindowTitle("AHARTS - Troubleshooting")
-        self.layout = qtw.QVBoxLayout(self)
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.setWindowIcon(QtGui.QIcon(tsystem_icon()))
 
+        # Store a reference to the main form
+        self.main_window = main_window
+        
         # Header
-        header = qtw.QLabel("Anthony's Home Appliance-Repair Ticketing System")
-        header.setFont(qtg.QFont('Arial', 25))
+        header = QtWidgets.QLabel("Anthony's Home Appliance-Repair Ticketing System")
+        header.setFont(QtGui.QFont('Arial', 25))
         self.layout.addWidget(header)
 
         # Troubleshooting information section
-        troubleshoot_header = qtw.QLabel("Troubleshooting Information:")
-        troubleshoot_header.setFont(qtg.QFont('Arial', 15))
+        troubleshoot_header = QtWidgets.QLabel("Troubleshooting Information:")
+        troubleshoot_header.setFont(QtGui.QFont('Arial', 15))
         self.layout.addWidget(troubleshoot_header)
 
-        form_layout = qtw.QFormLayout()
+        form_layout = QtWidgets.QFormLayout()
 
         # Service Ticket Number
-        serv_ticket = qtw.QLabel("Service-Ticket Number:")
-        serv_ticket.setFont(qtg.QFont('Arial', 9))
-        serv_ticket_entry = qtw.QLineEdit()
+        serv_ticket = QtWidgets.QLabel("Service-Ticket Number:")
+        serv_ticket.setFont(QtGui.QFont('Arial', 9))
+        serv_ticket_entry = QtWidgets.QLineEdit()
         serv_ticket_entry.setPlaceholderText("Input Service-Ticket number here...")
         serv_ticket_entry.setText(pyperclip.paste())
         form_layout.addRow(serv_ticket, serv_ticket_entry)
@@ -39,45 +42,45 @@ class TroubleshootingWindow(qtw.QWidget):
         self.layout.addLayout(form_layout)
 
         # What's customer full name label
-        cust_ticket_lbl = qtw.QLabel("What's the customer Service-Ticket number?")
-        cust_ticket_lbl.setFont(qtg.QFont('Arial', 12))
+        cust_ticket_lbl = QtWidgets.QLabel("What's the customer Service-Ticket number?")
+        cust_ticket_lbl.setFont(QtGui.QFont('Arial', 12))
         self.layout.addWidget(cust_ticket_lbl)
 
         # Customer information for this ticket
-        cust_info_lbl = qtw.QLabel("Customer Name: Ipsum Dolor")
-        cust_info_lbl.setFont(qtg.QFont('Arial', 12))
+        cust_info_lbl = QtWidgets.QLabel("Customer Name: Ipsum Dolor")
+        cust_info_lbl.setFont(QtGui.QFont('Arial', 12))
         self.layout.addWidget(cust_info_lbl)
 
         # Textbox placeholders
-        service_info = qtw.QComboBox()
+        service_info = QtWidgets.QComboBox()
         service_info.addItems([service.strip() for service in services()])
 
-        broken_comp_txtbox = qtw.QLineEdit()
+        broken_comp_txtbox = QtWidgets.QLineEdit()
         broken_comp_txtbox.setPlaceholderText("Fuse, Wiring, Capacitor etc.")
 
-        quantity_txtbox = qtw.QLineEdit()
+        quantity_txtbox = QtWidgets.QLineEdit()
         quantity_txtbox.setPlaceholderText("Number only (1, 2, 10)")
 
-        price_txtbox = qtw.QLineEdit()
+        price_txtbox = QtWidgets.QLineEdit()
         price_txtbox.setPlaceholderText("Number only (50, 150, 300)")
 
-        form_layout = qtw.QFormLayout()
-        form_layout.addRow(qtw.QLabel("Type of Service:"), service_info)
-        form_layout.addRow(qtw.QLabel("Broken Component:"), broken_comp_txtbox)
-        form_layout.addRow(qtw.QLabel("Quantity:"), quantity_txtbox)
-        form_layout.addRow(qtw.QLabel("Price:"), price_txtbox)
+        form_layout = QtWidgets.QFormLayout()
+        form_layout.addRow(QtWidgets.QLabel("Type of Service:"), service_info)
+        form_layout.addRow(QtWidgets.QLabel("Broken Component:"), broken_comp_txtbox)
+        form_layout.addRow(QtWidgets.QLabel("Quantity:"), quantity_txtbox)
+        form_layout.addRow(QtWidgets.QLabel("Price:"), price_txtbox)
 
         self.layout.addLayout(form_layout)
 
         # Buttons to modify data in table
-        add_button = qtw.QPushButton("Add")
+        add_button = QtWidgets.QPushButton("Add")
         add_button.clicked.connect(lambda: add_data(service_info.currentText(), broken_comp_txtbox.text(), quantity_txtbox.text(), price_txtbox.text(), table_widget))
 
-        remove_button = qtw.QPushButton("Remove")
+        remove_button = QtWidgets.QPushButton("Remove")
         remove_button.clicked.connect(lambda: remove_data(table_widget))
 
         # Create a layout for the buttons
-        buttons_layout = qtw.QHBoxLayout()
+        buttons_layout = QtWidgets.QHBoxLayout()
         buttons_layout.addStretch()
         buttons_layout.addWidget(add_button)
         buttons_layout.addWidget(remove_button)
@@ -86,13 +89,13 @@ class TroubleshootingWindow(qtw.QWidget):
         form_layout.addRow(buttons_layout)
 
         # Create a table widget to display the transferred data with column headers
-        table_widget = qtw.QTableWidget()
+        table_widget = QtWidgets.QTableWidget()
         table_widget.setColumnCount(4)
         table_widget.setHorizontalHeaderLabels(["Service", "Broken Component", "Quantity", "Price"])
 
         # Set the column resize mode to stretch
         header = table_widget.horizontalHeader()
-        header.setSectionResizeMode(qtw.QHeaderView.Stretch)
+        header.setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
 
         self.layout.addWidget(table_widget)
 
@@ -100,10 +103,10 @@ class TroubleshootingWindow(qtw.QWidget):
         def add_data(service, broken_component, quantity, price, table_widget):
             row_count = table_widget.rowCount()
             table_widget.insertRow(row_count)
-            table_widget.setItem(row_count, 0, qtw.QTableWidgetItem(service))
-            table_widget.setItem(row_count, 1, qtw.QTableWidgetItem(broken_component))
-            table_widget.setItem(row_count, 2, qtw.QTableWidgetItem(quantity))
-            table_widget.setItem(row_count, 3, qtw.QTableWidgetItem(price))
+            table_widget.setItem(row_count, 0, QtWidgets.QTableWidgetItem(service))
+            table_widget.setItem(row_count, 1, QtWidgets.QTableWidgetItem(broken_component))
+            table_widget.setItem(row_count, 2, QtWidgets.QTableWidgetItem(quantity))
+            table_widget.setItem(row_count, 3, QtWidgets.QTableWidgetItem(price))
 
         # Remove Data inside the Table
         def remove_data(table_widget):
@@ -111,28 +114,51 @@ class TroubleshootingWindow(qtw.QWidget):
             if selected_row >= 0:
                 table_widget.removeRow(selected_row)
 
-        # BUTTONS ##########################################################################################
+        #########################################################################
+        # BUTTONS
+        #########################################################################
+
+        # Draw a horizontal line
+        self.layout.addWidget(QFrame(self, frameShape=QFrame.HLine, frameShadow=QFrame.Sunken, lineWidth=1))
+
+        # create a QHBoxLayout to hold the buttons
+        buttons_layout = QHBoxLayout()
 
         # Save button
-        cust_save_button = qtw.QPushButton("Save")
-        self.layout.addWidget(cust_save_button)
+        save_button = QPushButton("Save")
+        buttons_layout.addWidget(save_button)
 
-        # Clear all fields button
-        cust_clear_button = qtw.QPushButton("Clear All")
-        self.layout.addWidget(cust_clear_button)
+        # Clear all fields buttonl
+        clear_button = QPushButton("Clear All")
+        buttons_layout.addWidget(clear_button)
+
+        # add the buttons layout to the main layout
+        self.layout.addLayout(buttons_layout)
+
+        # create a QHBoxLayout to hold the buttons
+        buttons_layout = QHBoxLayout()
 
         # Open database button
-        cust_db_button = qtw.QPushButton("View Customer Database")
-        self.layout.addWidget(cust_db_button)
+        serviceticket_db_btn = QPushButton("View Service-Ticket Database", clicked = lambda: open_serviceticket_database_viewer(self))
+        buttons_layout.addWidget(serviceticket_db_btn)
 
-        # Back to Main button
-        to_main_button = qtw.QPushButton("Back to Main")
-        self.layout.addWidget(to_main_button)
+        # Go back to main page
+        goback_btn = QPushButton("Go Back", clicked = lambda: return_to_previous_page(self, main_window))
+        buttons_layout.addWidget(goback_btn)
+
+        # add the buttons layout to the main layout
+        self.layout.addLayout(buttons_layout)
 
         self.show()
 
+        # Removing clipboard history
+        pyperclip.copy('')
 
+    def closeEvent(self, event):
+        return_to_previous_page(self, self.main_window)
+
+'''
 if __name__ == '__main__':
-    app = qtw.QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     mw = TroubleshootingWindow()
-    sys.exit(app.exec_())
+    sys.exit(app.exec_())'''
