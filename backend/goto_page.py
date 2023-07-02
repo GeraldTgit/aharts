113d545b96dd123e21c7c42bb76ee873994a570c
GeraldTgit <117510768+GeraldTgit@users.noreply.github.com>
2023-07-02 15:56:10 +0800

    Update and rename goto_page.py to backend/goto_page.py

---
+++ backend/goto_page.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt5.QtWidgets import QMessageBox
+
+from backend.public_backend import *
+from backend.logs_generator import log_message
+
+def return_to_previous_page(self, main_window):
+    # Show the main form and hide the current open form
+    try:
+        main_window.show()
+        self.close()  # Close the current form
+    except Exception as e:
+        message = f"Error returning to main form: {e}"
+        log_message(message)
+        QMessageBox.information(None, "AHARTS", message) 
+        sys.exit()
