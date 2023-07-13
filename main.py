af6cd4b3ca84ec8fee9d848789e806fb3418f2d4
GeraldTgit <117510768+GeraldTgit@users.noreply.github.com>
2023-07-13 21:29:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ from troubleshooting import TroubleshootingWindow
 # Backend scripts
 from database_viewer import *
 from backend.logs_generator import *
-from backend.goto_page import *
 
 # Param checker
 setup_log_file()
@@ -22,7 +21,7 @@ current_time = datetime.datetime.now()
 current_hour = current_time.hour
 
 # Task bar icon
-ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid())
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
 if current_hour < 12:
     greetings = "Good morning!"
@@ -33,17 +32,20 @@ else:
 
 current_date = current_time.strftime("%A, %B %d, %Y")
 
+# Simply null
+null = ""
+
 class MainForm(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("AHARTS - Main Menu")
         self.setLayout(QVBoxLayout())
-        self.setWindowIcon(QIcon(tsystem_icon()))
+        self.setWindowIcon(QIcon(tsystem_icon))
 
         # Userform header
-        main_header = QLabel("Anthony's Home Appliance-Repair Ticketing System")
+        main_header = QLabel(my_main_header)
         main_header.setFont(QFont('Arial', 20))
-        self.layout().addWidget(main_header)
+        self.layout().addWidget(main_header, alignment=Qt.AlignCenter)
 
         # create a QHBoxLayout to hold the greeting_and_lbl_today_layout
         greeting_and_lbl_today_layout = QHBoxLayout()
@@ -71,79 +73,148 @@ class MainForm(QWidget):
         self.timer.timeout.connect(self.update_time)
         self.timer.start(1000) # update every 1 second
 
-        # Draw a horizontal line
+        # Draw a horizontal line -----------------------------------------------------------------------------
         self.layout().addWidget(QFrame(self, frameShape=QFrame.HLine, frameShadow=QFrame.Sunken, lineWidth=1))
 
         # Customer form header
         cust_header = QLabel("Customer Form")
         cust_header.setFont(QFont('Arial', 15))
-        self.layout().addWidget(cust_header)
+        self.layout().addWidget(cust_header, alignment=Qt.AlignCenter)
 
     
         #########################################################################
         # BUTTONS
         #########################################################################
+        # Set style for layouts
+        style_sheet_style = "QPushButton { text-align: center; }"
 
         # Create the button layout
         cust_buttons_layout = QHBoxLayout()
-        
+
         # Create a button to open the other user form
-        cust_entry_button = QPushButton("Add New Customer", clicked=lambda: self.open_addnew_custwindow())
-        cust_entry_icon = QIcon(icons() + "add_new_customer.png")
+        cust_entry_button = QPushButton(clicked=lambda: self.open_addnew_custwindow())
+        cust_entry_icon = QIcon(icons_dir + "add_new_customer.png")
         cust_entry_button.setIcon(cust_entry_icon)
-        cust_entry_button.setIconSize(QSize(24, 24))  # Adjust the icon size as needed
-        cust_entry_button.setFixedSize(250, 50)  # Set the desired button size
-        cust_buttons_layout.addWidget(cust_entry_button)
+        cust_entry_button.setIconSize(QSize(50, 50))  # Adjust the icon size as needed
+        cust_entry_button.setFixedSize(150, 80)  # Set the desired button size
 
-        # call aharts_edit_customer.py
-        cust_edit_button = QPushButton("Edit Customer Information", clicked = lambda: self.open_edit_custwindow())
-        edit_customer_icon = QIcon(icons()+"edit_customer.png")
+        # Set the button text alignment to center
+        cust_entry_button.setStyleSheet(style_sheet_style)
+
+        # Create a QVBoxLayout for the button
+        cust_entry_layout = QVBoxLayout()
+        cust_entry_layout.addWidget(cust_entry_button, alignment=Qt.AlignHCenter)
+        cust_entry_layout.addWidget(QLabel("Add New Customer", alignment=Qt.AlignHCenter))
+
+        # Create a QWidget for the button layout
+        cust_entry_widget = QWidget()
+        cust_entry_widget.setLayout(cust_entry_layout)
+
+        # Add the button widget to the main layout
+        cust_buttons_layout.addWidget(cust_entry_widget, alignment=Qt.AlignCenter)
+
+        # Create a button to edit customer information
+        cust_edit_button = QPushButton(clicked=lambda: self.open_edit_custwindow())
+        edit_customer_icon = QIcon(icons_dir + "edit_customer.png")
         cust_edit_button.setIcon(edit_customer_icon)
-        cust_edit_button.setIconSize(QSize(24, 24))  # Adjust the icon size as needed
-        cust_edit_button.setFixedSize(250, 50)  # Set the desired button size
-        cust_buttons_layout.addWidget(cust_edit_button)
+        cust_edit_button.setIconSize(QSize(50, 50))  # Adjust the icon size as needed
+        cust_edit_button.setFixedSize(150, 80)  # Set the desired button size
 
-        # add the buttons layout to the main layout
+        # Set the button text alignment to center
+        cust_edit_button.setStyleSheet(style_sheet_style)
+
+        # Create a QVBoxLayout for the button
+        cust_edit_layout = QVBoxLayout()
+        cust_edit_layout.addWidget(cust_edit_button, alignment=Qt.AlignHCenter)
+        cust_edit_layout.addWidget(QLabel("Edit Customer Information", alignment=Qt.AlignHCenter))
+
+        # Create a QWidget for the button layout
+        cust_edit_widget = QWidget()
+        cust_edit_widget.setLayout(cust_edit_layout)
+
+        # Add the button widget to the main layout
+        cust_buttons_layout.addWidget(cust_edit_widget, alignment=Qt.AlignCenter)
+
+        # Add the buttons layout to the main layout
         self.layout().addLayout(cust_buttons_layout)
 
-        # Draw a horizontal line
+
+        # Draw a horizontal line -----------------------------------------------------------------------------
         self.layout().addWidget(QFrame(self, frameShape=QFrame.HLine, frameShadow=QFrame.Sunken, lineWidth=1))
 
         # Ticket form header
-        cust_header = QLabel("Ticket Form")
-        cust_header.setFont(QFont('Arial', 15))
-        self.layout().addWidget(cust_header)
+        ticket_form_header = QLabel("Ticket Form")
+        ticket_form_header.setFont(QFont('Arial', 15))
+        self.layout().addWidget(ticket_form_header, alignment=Qt.AlignCenter)
+
+        # Create the button layout
+        ts_app_buttons_layout = QHBoxLayout()
 
         # call aharts_ticket_entry.py
-        ticket_entry_button = QPushButton("Service Ticket", clicked = lambda: self.open_create_service_ticketwindow())
-        service_ticket_icon = QIcon(icons()+"service_ticket.png")
+        ticket_entry_button = QPushButton(clicked = lambda: self.open_create_service_ticketwindow())
+        service_ticket_icon = QIcon(icons_dir+"service_ticket.png")
         ticket_entry_button.setIcon(service_ticket_icon)
-        self.layout().addWidget(ticket_entry_button)
+        ticket_entry_button.setIconSize(QSize(50, 50))  # Adjust the icon size as needed
+        ticket_entry_button.setFixedSize(150, 80)  # Set the desired button size
+
+        # Set the button text alignment to center
+        ticket_entry_button.setStyleSheet(style_sheet_style)
+
+        # Create a QVBoxLayout for the button
+        ticket_entry_layout = QVBoxLayout()
+        ticket_entry_layout.addWidget(ticket_entry_button, alignment=Qt.AlignHCenter)
+        ticket_entry_layout.addWidget(QLabel("Service Ticket", alignment=Qt.AlignHCenter))
+
+        # Create a QWidget for the button layout
+        ticket_entry_widget = QWidget()
+        ticket_entry_widget.setLayout(ticket_entry_layout)
+
+        # Add the button widget to the main layout
+        ts_app_buttons_layout.addWidget(ticket_entry_widget, alignment=Qt.AlignCenter)
 
         # Appliance troubleshooting log
-        appliance_ts_log = QPushButton("Appliance Troubleshooting Log", clicked = lambda: self.open_troubleshootingwindow())
-        ts_order_icon = QIcon(icons()+"troubleshooting_order.png")
+        appliance_ts_log = QPushButton(clicked = lambda: self.open_troubleshootingwindow())
+        ts_order_icon = QIcon(icons_dir+"troubleshooting_order.png")
         appliance_ts_log.setIcon(ts_order_icon)
-        self.layout().addWidget(appliance_ts_log)
+        appliance_ts_log.setIconSize(QSize(50, 50))  # Adjust the icon size as needed
+        appliance_ts_log.setFixedSize(150, 80)  # Set the desired button size
 
-        # Draw a horizontal line
+        # Set the button text alignment to center
+        appliance_ts_log.setStyleSheet(style_sheet_style)
+
+        # Create a QVBoxLayout for the button
+        appliance_ts_log_layout = QVBoxLayout()
+        appliance_ts_log_layout.addWidget(appliance_ts_log, alignment=Qt.AlignHCenter)
+        appliance_ts_log_layout.addWidget(QLabel("Appliance Troubleshooting Order", alignment=Qt.AlignHCenter))
+
+        # Create a QWidget for the button layout
+        appliance_ts_log_widget = QWidget()
+        appliance_ts_log_widget.setLayout(appliance_ts_log_layout)
+
+        # Add the button widget to the main layout
+        ts_app_buttons_layout.addWidget(appliance_ts_log_widget, alignment=Qt.AlignCenter)
+
+        # Add the buttons layout to the main layout
+        self.layout().addLayout(ts_app_buttons_layout)
+
+        # Draw a horizontal line -----------------------------------------------------------------------------
         self.layout().addWidget(QFrame(self, frameShape=QFrame.HLine, frameShadow=QFrame.Sunken, lineWidth=1))
 
         # Databases
         database_header = QLabel("Databases")
         database_header.setFont(QFont('Arial', 15))
-        self.layout().addWidget(database_header)
+        self.layout().addWidget(database_header, alignment=Qt.AlignCenter)
 
 
         # Open database button
         cust_db_btn = QPushButton("View Customer Database", clicked = lambda: open_cust_database_viewer(self))
-        cust_db_icon = QIcon(icons()+"customer_database.png")
+        cust_db_icon = QIcon(icons_dir+"customer_database.png")
         cust_db_btn.setIcon(cust_db_icon)
         self.layout().addWidget(cust_db_btn)
 
         # Open database button
         serviceticket_db_btn = QPushButton("View Service-Ticket Database", clicked = lambda: open_serviceticket_database_viewer(self))
-        serviceticket_db_icon = QIcon(icons()+"service_ticket_database.png")
+        serviceticket_db_icon = QIcon(icons_dir+"service_ticket_database.png")
         serviceticket_db_btn.setIcon(serviceticket_db_icon)
         self.layout().addWidget(serviceticket_db_btn)
 
@@ -161,7 +232,7 @@ class MainForm(QWidget):
             app.quit()
             # Restart the application
             subprocess.Popen([sys.executable, __file__])
-
+ 
     def open_addnew_custwindow(self):
         # Show the customer form and hide the main form 
         addnew_custwindow = AddNewCustWindow(self)
@@ -173,13 +244,13 @@ class MainForm(QWidget):
         self.edit_custwindow.show()
         self.hide()
         
-    def open_create_service_ticketwindow(self):
-        self.create_service_ticketwindow = ServiceTicketWindow(self)
+    def open_create_service_ticketwindow(self):       
+        self.create_service_ticketwindow = ServiceTicketWindow(self,null)
         self.create_service_ticketwindow.show()
         self.hide()
        
     def open_troubleshootingwindow(self):
-        self.troubleshootingwindow = TroubleshootingWindow(self)
+        self.troubleshootingwindow = TroubleshootingWindow(self,null)
         self.troubleshootingwindow.show()
         self.hide()
                  
