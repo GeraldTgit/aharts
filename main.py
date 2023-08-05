a07abab145a4247fadacff7197dfefe3f5a22427
GeraldTgit <117510768+GeraldTgit@users.noreply.github.com>
2023-08-05 13:11:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ from add_new_cust import AddNewCustWindow
 from edit_customer import Edit_CustWindow
 from create_service_ticket import ServiceTicketWindow
 from troubleshooting import TroubleshootingWindow
+from receipt import open_receipt
 
 # Backend scripts
 from database_viewer import *
 from backend.logs_generator import *
 from backend.main_backend import *
 
-# Param checker
+# Param checker and preparations
 setup_log_file()
 check_customer_db()
 check_serv_ticket_db()
@@ -35,10 +36,40 @@ else:
 
 current_date = current_time.strftime("%A, %B %d, %Y")
 
+username = getpass.getuser()
+
 # Simply null
 null = ""
 
-class MainForm(QWidget):
+
+# SPLASH SCREEN 
+def show_splash_screen(width, height):
+    # Create a splash screen widget
+    splash = QWidget()
+    splash.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
+    splash.setWindowTitle("tsystem")
+    splash.setWindowIcon(QIcon(tsystem_icon))
+
+    # Create a QLabel to display the animated GIF
+    splash_label = QLabel(splash)
+    splash_label.setAlignment(Qt.AlignCenter)
+    movie = QMovie(splash_gif)  # Replace "animation.gif" with the path to your animated GIF
+    movie.frameChanged.connect(lambda frame: splash_label.setPixmap(movie.currentPixmap()))
+    movie.start()
+
+    # Calculate the position and size of the splash screen
+    desktop = QDesktopWidget().screenGeometry()
+    x = desktop.width() // 2 - width // 2
+    y = desktop.height() // 2 - height // 2
+    splash.setGeometry(x, y, width, height)
+
+    # Show the splash screen
+    splash.show()
+
+    return splash, movie
+
+# MainWindow Form
+class MainForm(QWidget):   
     def __init__(self):
         super().__init__()
         self.setWindowTitle("AHARTS - Main Menu")
@@ -66,15 +97,27 @@ class MainForm(QWidget):
         # add the greeting_and_lbl_today_layout layout to the main layout
         self.layout().addLayout(greeting_and_lbl_today_layout)
 
-        # create a QLabel object to display the time
+        # Create a QHBoxLayout to hold the username and current time labels
+        username_currenttime_layout = QHBoxLayout()
+
+        # Create the label to display the current user
+        current_user = QLabel(f'Login as: {username}')
+        current_user.setFont(QFont('Arial', 9))
+        username_currenttime_layout.addWidget(current_user)
+
+        # Create the label to display the current time
         self.lbl_current_time = QLabel(self)
         self.lbl_current_time.setFont(QFont('Arial', 9))
-        self.layout().addWidget(self.lbl_current_time, alignment=Qt.AlignTop | Qt.AlignRight)
+        username_currenttime_layout.addStretch(1)  # Add a stretchable space to push the labels to the right
+        username_currenttime_layout.addWidget(self.lbl_current_time)
 
-        # start the timer to update the label
+        # Add the QHBoxLayout to the main layout
+        self.layout().addLayout(username_currenttime_layout)
+
+        # Start the timer to update the label
         self.timer = QTimer()
         self.timer.timeout.connect(self.update_time)
-        self.timer.start(1000) # update every 1 second
+        self.timer.start(1000) # Update every 1 second
 
         # Draw a horizontal line -----------------------------------------------------------------------------
         self.layout().addWidget(QFrame(self, frameShape=QFrame.HLine, frameShadow=QFrame.Sunken, lineWidth=1))
@@ -87,7 +130,7 @@ class MainForm(QWidget):
         #########################################################################
         # BUTTONS
         #########################################################################
-        # Set style for layouts
+        # Set style for buttons layouts
         style_sheet_style = "QPushButton { text-align: center; }"
 
         # Create the button layout
@@ -207,9 +250,6 @@ class MainForm(QWidget):
         self.layout().addWidget(database_header, alignment=Qt.AlignCenter)
 
         # create a QHBoxLayout to hold the buttons
-        #db_buttons_layout = QHBoxLayout()
-
-        # create a QHBoxLayout to hold the buttons
         db_buttons_layout = QHBoxLayout()
 
         # Open database button
@@ -232,12 +272,15 @@ class MainForm(QWidget):
         # add the QHBoxLayout to the main layout
         self.layout().addLayout(db_buttons_layout)
 
-        
+        receipt_btn = QPushButton("receipt", clicked = lambda: open_receipt(self))
+        receipt_btn.setFont(QFont('Arial', 15))
+        self.layout().addWidget(receipt_btn)
+
         # refresh page
         reload_button = QPushButton("RELOAD PAGE", clicked = lambda: reload_main())
         reload_button.setFont(QFont('Arial', 15))
         self.layout().addWidget(reload_button)
-        
+
         
         ########################################################
         # FUNCTIONS
@@ -247,7 +290,7 @@ class MainForm(QWidget):
             app.quit()
             # Restart the application
             subprocess.Popen([sys.executable, __file__])
- 
+
     def open_addnew_custwindow(self):
         # Show the customer form and hide the main form 
         addnew_custwindow = AddNewCustWindow(self)
@@ -263,12 +306,14 @@ class MainForm(QWidget):
         self.create_service_ticketwindow = ServiceTicketWindow(self,null)
         self.create_service_ticketwindow.show()
         self.hide()
-       
+    
     def open_troubleshootingwindow(self):
         self.troubleshootingwindow = TroubleshootingWindow(self,null)
         self.troubleshootingwindow.show()
         self.hide()
-                 
+
+    
+                
     # This code updates lbl_current_time every seconds  
     def update_time(self):
         # get the current time as a string
@@ -290,14 +335,32 @@ class MainForm(QWidget):
         else:
             event.ignore()
 
+
 try:
 
-    # Main application
-    if __name__ == '__main__':
-        app = QApplication(sys.argv)
+    app = QApplication(sys.argv)
+
+    # Set the desired dimensions for the splash screen
+    splash_width = 500
+    splash_height = 470
+
+    # Show the splash screen
+    splash, movie = show_splash_screen(splash_width, splash_height)
+
+    # Delay the appearance of the MainWindow
+    main_window_delay = QTimer()
+    main_window_delay.setInterval(2000)  # Adjust the delay as needed (in milliseconds)
+
+    def show_main_window():
         main_form = MainForm()
         main_form.show()
-        sys.exit(app.exec_())
+        main_window_delay.stop()
+        splash.close()
+
+    main_window_delay.timeout.connect(show_main_window)
+    main_window_delay.start()
+
+    sys.exit(app.exec_())
 
 except Exception as e:
     # Log the exception message and traceback
