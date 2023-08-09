77bee1ef28c45fba0bb87d1e46d3f41b99f17465
GeraldTgit <117510768+GeraldTgit@users.noreply.github.com>
2023-08-09 09:38:27 +0800

    Update create_service_ticket.py

--- create_service_ticket.py
+++ create_service_ticket.py
@@ -1,7 +1,6 @@
 # Anthony's Home Appliance Repair Ticketing System
-from PyQt5.QtWidgets import QApplication, QVBoxLayout, QLabel, QLineEdit, QPushButton, QComboBox, QFrame, QFormLayout, QHBoxLayout, QWidget, QMessageBox
-from PyQt5.QtGui import QFont, QIcon
-from PyQt5.QtCore import *
+# Common packages and customize scripts
+from backend.public_backend import *
 
 # Sub-pages
 from troubleshooting import TroubleshootingWindow
@@ -9,44 +8,24 @@ from troubleshooting import TroubleshootingWindow
 # Customize scripts
 from database_viewer import *
 from backend.logs_generator import *
-from backend.public_backend import *
 from backend.goto_page import return_to_previous_page
 from backend.service_ticket_backend import *
 
 class ServiceTicketWindow(QWidget):
-    def __init__(self, main_window):
+    def __init__(self, main_window,recent_txn_id):
         super().__init__()
         self.setWindowTitle("AHARTS - Create Service Ticket")
         self.layout = QVBoxLayout(self)
-        self.setWindowIcon(QtGui.QIcon(tsystem_icon()))
+        self.setWindowIcon(QIcon(tsystem_icon))
 
         # Store a reference to the main form
         self.main_window = main_window
-
-        # Look for customer information based on tracking number
-        def look_up_cust():     
-            #write_mode(cust_tnum_input)      
-            # Get the search query from the entry box
-            search_query = cust_tnum_input.text()
-            # Open the CSV file and search for the customer information
-            with open(customer_db(), 'r') as file:
-                reader = csv.reader(file)
-                for row in reader:
-                    if search_query in row:
-                        cust_tnum.setText("Tracking Number:")
-                        # Customer information found, show in the textboxes
-                        cust_fullname_lbl.setText(f"Customer Name: {row[1]} {row[2]}")
-                        #cust_email_entry.setText(row[4])
-
-                        break
-                                     
-                    cust_fullname_lbl.setText("Customer Information Not Found")
-
-
+        self.recent_txn_id = str(recent_txn_id)
+             
         # Header
-        title_header = QLabel("Anthony's Home Appliance-Repair Ticketing System")
-        title_header.setFont(QFont('Arial', 25))
-        self.layout.addWidget(title_header)
+        main_header = QLabel(my_main_header)
+        main_header.setFont(QFont('Arial', 20))
+        self.layout.addWidget(main_header)
 
         # Customer Information Section
         cust_info_header = QLabel("Customer Information:")
@@ -60,11 +39,12 @@ class ServiceTicketWindow(QWidget):
         cust_tnum.setFont(QFont('Arial', 9))
         cust_tnum_input = QLineEdit()
         cust_tnum_input.setPlaceholderText("Input customer tracking number here...")
-        cust_tnum_input.textChanged.connect(look_up_cust)
+        cust_tnum_input.textChanged.connect(lambda: look_up_cust())
         form_layout.addRow(cust_tnum, cust_tnum_input)
 
         self.layout.addLayout(form_layout)
 
+    
         # What's customer full name label
         cust_fullname_lbl = QLabel("What's the customer tracking number?")
         cust_fullname_lbl.setFont(QFont('Arial', 12))
@@ -78,6 +58,7 @@ class ServiceTicketWindow(QWidget):
         # Drop down options for types of appliances
         appliance_combo_box = QComboBox()
         appliance_combo_box.addItems([appliance.strip() for appliance in appliances()])
+        appliance_combo_box.setEditable(True)
 
         # Textbox placeholder
         brand_input = QLineEdit()
@@ -89,11 +70,15 @@ class ServiceTicketWindow(QWidget):
         issue_input = QLineEdit()
         issue_input.setPlaceholderText("Not responding")
 
+        status_combo_box = QComboBox()
+        status_combo_box.addItems([status.strip() for status in service_status()])
+        
         form_layout = QFormLayout()
         form_layout.addRow(QLabel("Type of Appliance:"), appliance_combo_box)
         form_layout.addRow(QLabel("Brand:"), brand_input)
         form_layout.addRow(QLabel("Model:"), model_input)
         form_layout.addRow(QLabel("Issue:"), issue_input)
+        form_layout.addRow(QLabel("Service Status:"), status_combo_box)
 
         self.layout.addLayout(form_layout)
 
@@ -126,8 +111,8 @@ class ServiceTicketWindow(QWidget):
         buttons_layout = QHBoxLayout()
 
         # Open database button
-        serviceticket_db_btn = QPushButton("View Service-Ticket Database", clicked = lambda: open_serviceticket_database_viewer(self))
-        buttons_layout.addWidget(serviceticket_db_btn)
+        cust_db_btn = QPushButton("View Customer Database", clicked = lambda: open_cust_database_viewer(self))
+        buttons_layout.addWidget(cust_db_btn)
 
         # Go back to main page
         goback_btn = QPushButton("Go Back", clicked = lambda: return_to_previous_page(self, main_window))
@@ -142,6 +127,11 @@ class ServiceTicketWindow(QWidget):
         # FUNCTIONS
         ########################################################
 
+        # Look for customer information (name) based on tracking number
+        def look_up_cust():
+            customer_name = look_up_cust_name(validate_num_text(cust_tnum_input))
+            cust_fullname_lbl.setText(f"Customer Name: <b>{customer_name}</b>")  
+
         def create_ticket():           
             msgBox = QMessageBox()
             msgBox.setIcon(QMessageBox.Question)
@@ -154,9 +144,10 @@ class ServiceTicketWindow(QWidget):
 
             if returnValue == QMessageBox.Ok:
                 serv_info = [serv_data.text() for serv_data in textbox_widgets]
-                serv_info.insert(1, appliance_combo_box.currentText())
-                save_new_ticket(serv_info)
-                self.open_troubleshootingwindow()
+                serv_info.insert(1,appliance_combo_box.currentText())
+                serv_info.append(status_combo_box.currentText())
+                recent_txn_id = save_new_ticket(serv_info)
+                self.open_troubleshootingwindow(recent_txn_id)
 
         def clear_textbox():
             for textbox in textbox_widgets:
@@ -164,17 +155,18 @@ class ServiceTicketWindow(QWidget):
 
             cust_fullname_lbl.setText("What's the customer tracking number?")
 
+
         # Transfer/set latest customer id
-        cust_tnum_input.setText(pyperclip.paste())
-        pyperclip.copy('')
+        cust_tnum_input.setText(self.recent_txn_id)
 
     #######################################d#################
     # MORE FUNCTIONS
     ########################################################
+    
 
-    def open_troubleshootingwindow(self):
+    def open_troubleshootingwindow(self,recent_txn_id):
         self.hide()
-        self.troubleshootingwindow = TroubleshootingWindow(self)
+        self.troubleshootingwindow = TroubleshootingWindow(self,recent_txn_id)
         self.troubleshootingwindow.show()
 
     def closeEvent(self, event):
