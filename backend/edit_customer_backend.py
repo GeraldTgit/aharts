86c70f9fed0fe1279f4ab3ee1b9d8fea40d64d06
GeraldTgit <117510768+GeraldTgit@users.noreply.github.com>
2023-08-09 09:45:02 +0800

    Update edit_customer_backend.py

--- backend/edit_customer_backend.py
+++ backend/edit_customer_backend.py
@@ -1,21 +1,12 @@
 # Anthony's Home Appliance Repair Ticketing System
-from PyQt5.QtWidgets import QMessageBox
-import tempfile
-import datetime
-import shutil
-import os
-import csv
+from backend.public_backend import *
 
 # Customize scripts
 from backend.logs_generator import *
 from backend.add_new_customer_backend import *
-from backend.public_backend import *
 from backend.goto_page import *
 
 # SETUP EVERYTHING FIRST
-
-# Customer database header
-headers = ['Customer ID', 'First Name', 'Last Name', 'Contact Number', 'Email', 'Home Address', 'ID Type', 'ID Path']
 
 def upload_identification(id_path,update):
     # Slicing identification label to get the id absolute path only
@@ -26,15 +17,16 @@ def upload_identification(id_path,update):
     id_path = id_path[start_index + 1 : end_index]
     id_path=id_path.replace('"','')
 
-    # Cancelling upload if id not updated
-    if update == "no":
-        return id_path
-
-    destination_folder = temp_db()+"id/"
-                    
     # Get the filename and extension from the source path
     file_name = os.path.basename(id_path)
     file_name, file_extension = os.path.splitext(file_name)
+
+    # Cancelling upload if id not updated
+    if update == "no":
+        file_name = file_name.replace(' ','_')
+        return file_name+file_extension
+
+    destination_folder = id_db_dir
 
     # Generate the new filename with the current date and time
     current_date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
@@ -47,45 +39,53 @@ def upload_identification(id_path,update):
     destination_path=destination_path.replace(' ','_')                   
     try:
         shutil.copy2(id_path, destination_path)
-        log_message(f"Identification updated: {destination_path}")
-        return destination_path
+        log_message(f"Identification updated: {new_file_name}")
+        return new_file_name
     except:
         pass
 
-# Check if the customer.csv file exists in the customer_db directory
-def check_customer_db():
-    if not os.path.isfile(customer_db()):
-        # Create a new customer.csv file with headers
-        with open(customer_db(), 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(headers)
-            log_message("Customer database created.")
 
-def save_update(customer_info):         
-    # Rest of the code to update or append rows
-    with open(customer_db(), 'r') as file:
-        reader = csv.reader(file)
-        rows = list(reader)
-        
-        if len(rows) > 0:
-            for row in rows:
-                if row[0] == customer_info[0]:
-                    # Customer ID already exists, update the row
-                    before_update_cust_info = row[:8]
-                    after__update_cust_info = customer_info[:8]
-                    row[1:8] = customer_info[1:8]
-                    break
+def look_up_cust_info(search_query):
+    # Read the Parquet file into a DataFrame
+    df = pd.read_parquet(customer_db_dir)
 
-    # Write the modified data back to the file
-    with tempfile.NamedTemporaryFile(mode='w', delete=False) as temp_file:
-        with open(temp_file.name, 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerows(rows)  # Write the rows
+    # Filter the DataFrame based on the search query
+    rows = df[df['Customer ID'] == search_query]
 
-    # Replace the original file with the updated file
-    shutil.move(temp_file.name, customer_db())
-    
+    if not rows.empty:
+        # Customer information found, retrieve the first row
+        row = rows.iloc[0]
+
+        cust_info = [row['First Name'], row['Last Name'], row['Contact Number'], row['Email Address'], row['Home Address'], row['Presented ID'], row['Identification']]
+        return cust_info
+    else:
+        return []
+
+
+# Save update
+def save_update(customer_info):   
+    # Read the Parquet file into a DataFrame
+    df = pd.read_parquet(customer_db_dir)
+
+    customer_id = int(customer_info[0])
+
+    # Filter the DataFrame based on the customer ID
+    rows = df[df['Customer ID'] == customer_id]
+
+    if not rows.empty:
+        # Customer ID already exists, update the row
+        before_update_cust_info = rows.iloc[0].tolist()[:8]
+        after_update_cust_info = customer_info[:8]
+        df.loc[rows.index, 'First Name':'Identification'] = customer_info[1:9]
+    else:
+        # Customer ID does not exist, append a new row
+        new_row = pd.DataFrame([customer_info], columns=df.columns)
+        df = df.append(new_row, ignore_index=True)
+
+    # Write the updated DataFrame back to the Parquet file
+    df.to_parquet(customer_db_dir, index=False)
+
     # Prompt message and log
-    message = f"Customer information updated : \n FROM: {before_update_cust_info} \n TO: {after__update_cust_info}"
-    QMessageBox.information(None, "AHARTS", message)        
+    message = f"Customer information updated:\nFROM: {before_update_cust_info}\nTO: {after_update_cust_info}"
+    QMessageBox.information(None, "AHARTS", message)
     log_message(message)
