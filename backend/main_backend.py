96b528f9659ef00f820e1c0a1f446b503fb551ba
GeraldTgit <117510768+GeraldTgit@users.noreply.github.com>
2023-08-09 09:46:14 +0800

    Update main_backend.py

--- backend/main_backend.py
+++ backend/main_backend.py
@@ -1,5 +1,7 @@
 from backend.public_backend import *
 from backend.logs_generator import *
+from backend.add_new_customer_backend import save_new
+from backend.service_ticket_backend import save_new_ticket
 
 # Parameter file checking
 # Check if the customer.csv file exists in the customer_db_dir directory
@@ -23,3 +25,14 @@ def check_troubleshooting_order_db():
         df = pd.DataFrame(columns=ts_order_db_headers)
         df.to_parquet(troubleshooting_order_db_dir)
         log_message('Troubleshooting Order database created.')
+
+
+def create_dummy_transaction():
+    # Create dummy/guest customer
+    guest = ['Guest','','','','','','']
+    recent_txn_id = save_new(guest)
+
+    # Create dummy/service ticket
+    serv_info = [str(recent_txn_id),'','','','','']
+    recent_txn_id = save_new_ticket(serv_info)
+    return recent_txn_id
