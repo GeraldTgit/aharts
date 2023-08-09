6397731d4ccd965b7d68b1c78ff8634e78091dbb
GeraldTgit <117510768+GeraldTgit@users.noreply.github.com>
2023-08-09 09:46:36 +0800

    Update public_backend.py

--- backend/public_backend.py
+++ backend/public_backend.py
@@ -1,10 +1,15 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+from fpdf import FPDF
 import subprocess
 import traceback
 import tempfile
 import datetime
+import getpass
 import psutil
 import shutil
 import ctypes
@@ -42,68 +47,103 @@ temp_db_dir =  pwd_()+"temp_db/"
 # id directory
 id_db_dir = temp_db_dir+"id/"
 
+# Receipt directory
+receipt_db_dir = temp_db_dir+"receipt/"
+
 # customer database absolute path
-customer_db_dir = temp_db_dir+"customer.csv"
+customer_db_dir = temp_db_dir+"customer.parquet"
 
 # service ticket database absolute path
-service_ticket_db_dir = temp_db_dir+"service_ticket.csv"
+service_ticket_db_dir = temp_db_dir+"service_ticket.parquet"
 
 # troubleshooting order
-troubleshooting_order_db_dir = temp_db_dir+"troubleshooting_order.csv"
-
+troubleshooting_order_db_dir = temp_db_dir+"troubleshooting_order.parquet"
 
 # BEAUTIFICATIONS
 # id_type list absolute path
-tsystem_icon = pwd_()+"tsystem.ico"
+tsystem_icon = icons_dir+"tsystem.ico"
+
+splash_gif = icons_dir+"splash_screen.gif"
 
 # Task bar icon
 myappid = 'tsystem.aharts.ticketingsystem.1'  # Replace with a unique identifier
+
+# Dash line style
+dash_line_style = "border: none; border-bottom: 1px dashed black;"
+
+# Default error message
+default_err_msg = "Something went wrong. Please contact the developer"
+
+# DATABASE HEADERS
+
+# Customer Database Column Headers
+cust_db_headers = ['Customer ID', 'First Name', 'Last Name', 'Contact Number', 'Email Address', 'Home Address', 'Presented ID', 'Identification']
+
+
+# Service-Ticket database header
+service_ticket_db_headers = ['Service-Ticket ID', 'Customer ID', 'Appliance', 'Brand', 'Model', 'Issue', 'Status']
+
+#Troubleshooting Order table widget and db headers
+table_widget_headers = ["Service", "Component", "Quantity", "Price", "Subtotal"]
+ts_order_db_headers = ["Order ID","Service-Ticket ID", "Customer ID"] + table_widget_headers
 
 
 ########################################################
 # FUNCTIONS
 ########################################################
-    
+#Look up Customer ID second column based on Service-Ticket ID first column
 def look_up_cust_tracking_id(service_ticket_id):
-    # Open the CSV file and search for the customer information
-    with open(service_ticket_db_dir, 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            if service_ticket_id in row:
-                return row[1]
-                break
-        else:
-            return None
+    # Read the Parquet file into a DataFrame
+    df = pd.read_parquet(service_ticket_db_dir)
 
-@lru_cache(maxsize=None)
+    # Search for the customer information
+    row = df[df['Service-Ticket ID'] == service_ticket_id]
+    if not row.empty:
+        return row.iloc[0]['Customer ID']
+    else:
+        return None
+
 def look_up_appliance(service_ticket_id):
-    # Open the CSV file and search for the customer information
-    with open(service_ticket_db_dir, 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            if service_ticket_id in row:
-                return row[2]
-                break
-        else:
-            return "No Service-Ticket Found"        
+    # Read the Parquet file into a DataFrame
+    df = pd.read_parquet(service_ticket_db_dir)
 
-@lru_cache(maxsize=None)
+    # Search for the customer information
+    row = df[df['Service-Ticket ID'] == service_ticket_id]
+    if not row.empty:
+        return f"{row.iloc[0]['Appliance']} [{row.iloc[0]['Brand']} {row.iloc[0]['Model']}]"
+    else:
+        return "No Service-Ticket Found"        
+    
+def look_up_issue(service_ticket_id):
+    # Read the Parquet file into a DataFrame
+    df = pd.read_parquet(service_ticket_db_dir)
+
+    # Search for the customer information
+    row = df[df['Service-Ticket ID'] == service_ticket_id]
+    if not row.empty:
+        return f"{row.iloc[0]['Issue']}"
+    else:
+        return "No Service-Ticket Found"        
+    
+
 def look_up_cust_name(customer_tracking_id): 
-    # Open the CSV file and search for the customer information
-    with open(customer_db_dir, 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            if customer_tracking_id in row:
-                return f"{row[1]} {row[2]}"
-                break
-        else:
-            return "No Information Found"
+    # Read the Parquet file into a DataFrame
+    df = pd.read_parquet(customer_db_dir)
 
-@lru_cache(maxsize=None)
-def look_up_ts_order_data(service_ticket_id):
-    # Load Troubleshooting order base on service_ticket_id
-    with open(troubleshooting_order_db_dir, 'r') as file:
-        reader = csv.reader(file)
-        # Append order id to the rest of order details
-        data_rows = [[row[0]]+row[3:] for row in reader if row and row[1] == service_ticket_id]
-        return data_rows
+    # Search for the customer information
+    row = df[df['Customer ID'] == customer_tracking_id]
+    if not row.empty:
+        return f"{row.iloc[0]['First Name']} {row.iloc[0]['Last Name']}"
+    else:
+        return "No Information Found"
+
+
+# Validate if widget is numerical only return integer
+def validate_num_text(widget):
+    text = widget.text()
+    new_text = "".join(char for char in text if char.isdigit() or char == ".")
+    widget.setText(new_text)
+    try:
+        return int(new_text)
+    except:
+        pass
