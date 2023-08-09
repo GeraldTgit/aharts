46b83d455e7979da4824f92f33b3abd0e1c5c832
GeraldTgit <117510768+GeraldTgit@users.noreply.github.com>
2023-08-09 09:47:15 +0800

    Update service_ticket_backend.py

--- backend/service_ticket_backend.py
+++ backend/service_ticket_backend.py
@@ -1,83 +1,58 @@
-from PyQt5.QtWidgets import QMessageBox
-import os
-import csv
-import tempfile
-import shutil
-import pyperclip
-
 from backend.public_backend import *
 from backend.logs_generator import *
 
 # Types of Appliances
 def appliances():
-    with open(pwd_()+'/param/appliances.txt', 'r') as file:
+    with open(param_dir+'appliances.txt', 'r') as file:
         appliances = file.readlines()
 
     return appliances
 
+# Service status
+def service_status():
+    with open(param_dir+'service_status.txt', 'r') as file:
+        service_status = file.readlines()
 
-# Service-Ticket database header
-headers = ['Service-Ticket ID', 'Customer ID', 'Type of Appliance', 'Brand', 'Model', 'Issue']
+    return service_status
 
-def check_serv_ticket_db():
-    if not os.path.isfile(service_ticket_db()):
-        # Create a new service ticket CSV file with headers
-        with open(service_ticket_db(), 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(headers)
-            log_message("Service-Ticket database created.")
 
 def save_new_ticket(service_info):
-    check_serv_ticket_db()    
-    # Assigning customer tracking number
-    with open(service_ticket_db(), 'r') as file:
-        reader = csv.reader(file)
-        # skip header row if it exists
-        if csv.Sniffer().has_header(file.read(1024)):
-            file.seek(0)
-            next(reader)
+    # Read the Parquet file into a DataFrame
+    df = pd.read_parquet(service_ticket_db_dir)
 
-        try:
-            # Get the max value from the first column or assign 0 if the reader is empty
-            highest_ticket_id = max(int(row[0]) for row in reader)
-        except ValueError:
-            highest_ticket_id = 0
+    if len(df) > 0:
+        if 0 in df['Service-Ticket ID'].values:
+            # Service-Ticket ID already exists, do nothing
+            return
 
-    # Assign a value of 1 if no existing entries
+    # Convert the 'Service-Ticket ID' column to integer type
+    df['Service-Ticket ID'] = df['Service-Ticket ID'].astype(int)
+
+    # Assign a new Service-Ticket ID
+    highest_ticket_id = df['Service-Ticket ID'].max() if not df.empty else 0
     new_entry_ticket_id = highest_ticket_id + 1
-    pyperclip.copy(new_entry_ticket_id)
 
-    # Concatenate service information
-    service_info.insert(0,new_entry_ticket_id)
-    
-    # Rest of the code to update or append rows
-    with open(service_ticket_db(), 'r') as file:
-        reader = csv.reader(file)
-        rows = list(reader)
-        
-        if len(rows) > 0:
-            for row in rows:
-                if row[0] == 0:
-                    # Service-Ticket ID already exists, do nothing
-                    break
-            else:
-                # Service-Ticket ID doesn't exist, append a new row
-                rows.append([info for info in service_info])
-        else:
-            # Service-Ticket.csv is empty, add the headers and append a new row
-            rows.append(headers)
-            rows.append([info for info in service_info])
+    service_info.insert(0, new_entry_ticket_id)
 
-    # Write the modified data back to the file
-    with tempfile.NamedTemporaryFile(mode='w', delete=False) as temp_file:
-        with open(temp_file.name, 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerows(rows)  # Write the rows
+    # Create a new DataFrame with the new service ticket entry
+    new_entry = pd.DataFrame([service_info], columns=df.columns)
+
+    # Concatenate the new entry with the existing DataFrame
+    df = pd.concat([df, new_entry], ignore_index=True)
+
+    # Write the updated DataFrame back to the Parquet file
+    temp_file = tempfile.NamedTemporaryFile(delete=False)
+    temp_file.close()
+    df.to_parquet(temp_file.name)
 
     # Replace the original file with the updated file
-    shutil.move(temp_file.name, service_ticket_db())
-    
+    shutil.move(temp_file.name, service_ticket_db_dir)
+
     # Prompt message and log
-    message = f"New service ticket created : {service_info}"
-    QMessageBox.information(None, "AHARTS", message)        
+    message = f"New service ticket created: {service_info}"
+    QMessageBox.information(None, "AHARTS", message)
     log_message(message)
+
+    return new_entry_ticket_id
+
+
